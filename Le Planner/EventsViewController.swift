295d28e1710b667bb92b58c50f6ac997da851f1a
Abubakar Khan <abubakar609@gmail.com>
2018-01-22 14:21:49 +1100

    Initial Commit

---
+++ Le Planner/EventsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Le Planner
+//
+//  Created by Abubakar Khan on 22/1/18.
+//  Copyright © 2018 Abubakar Khan. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
