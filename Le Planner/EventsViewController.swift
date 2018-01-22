588004f7c365531b0800899f2b9be1573b902a81
Abubakar Khan <abubakar609@gmail.com>
2018-01-22 20:51:31 +1100

    Initial table population

--- Le Planner/EventsViewController.swift
+++ Le Planner/EventsViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UITableViewController {
 
+    let eventsArray = ["Party","Meeting","Conference","Work","Exam"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,17 @@ class EventsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableCell", for: indexPath)
+        cell.textLabel?.text = eventsArray[indexPath.row]
+        return cell
+    }
 
 }
 
