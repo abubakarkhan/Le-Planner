facba76ff36df9d65b830cfcd6499cfe9e7f9012
Abubakar Khan <abubakar609@gmail.com>
2018-01-23 11:30:32 +1100

    New launch screen

--- Le Planner/EventsViewController.swift
+++ Le Planner/EventsViewController.swift
@@ -10,30 +10,27 @@ import UIKit
 
 class EventsViewController: UITableViewController {
 
-    let eventsArray = ["Party","Meeting","Conference","Work","Exam"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return EventData.instance.getEventList().count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableCell", for: indexPath)
         //cell.textLabel?.text = eventsArray[indexPath.row]
-        let event = EventData.getInstance().eventList[0]
-        cell.textLabel?.text = event.title + " " + event.description
+        let event = EventData.instance.getEventList()[indexPath.row]
+        cell.textLabel?.text = event.title
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Print here fix
-        
-        print(eventsArray[indexPath.row])
+        print(EventData.instance.getEventList()[indexPath.row].title)
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
--- Le Planner/Model/EventData.swift
+++ Le Planner/Model/EventData.swift
@@ -10,25 +10,37 @@ import Foundation
 
 class EventData {
     
-    var eventList : Array<Event> = []
+    private var eventList : Array<Event> = []
     
-    private static var instance : EventData?
-    
-    static func getInstance() -> EventData {
-        if instance == nil {
-            instance = EventData()
-        }
-        return instance!
-    }
+    static let instance = EventData()
     
     private init(){
-        eventList.append(Event(title: "Yoga class",
-                               description: "Trying out yoga today with John",
-                               date: CLong(NSDate().timeIntervalSince1970*1000),
-                               time: CLong(NSDate().timeIntervalSince1970*1000)))
+        eventList.append(Event(
+            title: "Yoga class",
+            description: "Trying out yoga today with John",
+            date: CLong(NSDate().timeIntervalSince1970*1000),
+            time: CLong(NSDate().timeIntervalSince1970*1000)))
+        
+        eventList.append(Event(
+            title: "IOS Assignmnet 2",
+            description: "Meetup with the team member to discuess the future approach",
+            date: CLong(NSDate().timeIntervalSince1970*1000),
+            time: CLong(NSDate().timeIntervalSince1970*1000)))
+        
+        eventList.append(Event(
+            title: "Coffee Meetup",
+            description: "Meeting John for coffe",
+            date: CLong(NSDate().timeIntervalSince1970*1000),
+            time: CLong(NSDate().timeIntervalSince1970*1000)))
+        
+        eventList.append(Event(
+            title: "Meeting",
+            description: "Meeting Jack",
+            date: CLong(NSDate().timeIntervalSince1970*1000),
+            time: CLong(NSDate().timeIntervalSince1970*1000)))
     }
     
-    private func getEventList() -> Array<Event>{
+    func getEventList() -> Array<Event>{
         return eventList
     }
     
