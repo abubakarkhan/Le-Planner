9bad3a0a55bfade71503ef1f724815e83bb10615
Abubakar Khan <abubakar609@gmail.com>
2018-01-25 00:01:36 +1100

    After submission 1 commit

--- Le Planner/Model/NotesData.swift
+++ Le Planner/Model/NotesData.swift
@@ -19,7 +19,7 @@ class NotesData {
         
         notesList.append(Note(title: "Groceries", body: "Pick up egss,milk and break."))
         
-        notesList.append(Note(title: "Lorem Ipsum", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."))
+        notesList.append(Note(title: "Lectures and Labs", body: "Monday, Wednesday and Friday "))
         
         notesList.append(Note(title: "Lorem Ipsum", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."))
         
