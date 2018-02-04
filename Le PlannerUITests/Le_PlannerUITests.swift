3694b5f9a4c797d78c2aa162540f46d21c132739
Abubakar Khan <abubakar609@gmail.com>
2018-02-04 20:48:03 +1100

    Updated test cases

--- Le PlannerUITests/Le_PlannerUITests.swift
+++ Le PlannerUITests/Le_PlannerUITests.swift
@@ -19,53 +19,22 @@ class Le_PlannerUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    
-    
-    func testDeleteEvent() {
-        // Use recording to get started writing UI tests.
-        
-        let app = XCUIApplication()
-        app.tables["Empty list"].swipeDown()
-        app.tabBars.buttons["Events"].tap()
-        
-        let tablesQuery = app.tables
-        let meetupWithTheTeamMemberToDiscuessTheFutureApproachStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Meetup with the team member to discuess the future approach"]/*[[".cells.staticTexts[\"Meetup with the team member to discuess the future approach\"]",".staticTexts[\"Meetup with the team member to discuess the future approach\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        meetupWithTheTeamMemberToDiscuessTheFutureApproachStaticText.swipeDown()
-        meetupWithTheTeamMemberToDiscuessTheFutureApproachStaticText.swipeLeft()
-        tablesQuery.buttons["Delete"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Meeting John for coffe"]/*[[".cells.staticTexts[\"Meeting John for coffe\"]",".staticTexts[\"Meeting John for coffe\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeDown()
-        
-        
-        // Use check to see if event was deleted
-        XCTAssert(tablesQuery.cells.count == 5)
-    }
-    
-    func testDeleteNote() {
-        
-        let app = XCUIApplication()
-        app.tabBars.buttons["Notes"].tap()
-        
-        let tablesQuery = app.tables
-        let noteEntry = tablesQuery.staticTexts["Pick up egss,milk and break."]
-        noteEntry.swipeLeft()
-        tablesQuery.buttons["Delete"].tap()
-        
-        // Use check to see if note was deleted
-        XCTAssert(tablesQuery.cells.count == 3)
-       
-    }
-    
     func testAddNote(){
         
         let app = XCUIApplication()
         app.tabBars.buttons["Notes"].tap()
+        
+        let rowsBeforeAdding = app.tables.cells.count
+        
         app.navigationBars["Notes"].buttons["Add"].tap()
         
         let titleTextField = app.textFields["Title"]
         titleTextField.tap()
         titleTextField.typeText("This is a test")
         
-        let textView = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .textView).element
+        
+        let elementsQuery = app.scrollViews.otherElements
+        let textView = elementsQuery.children(matching: .textView).element
         textView.tap()
         textView.typeText("This is a test description")
         app.navigationBars["Add Note"].buttons["Add"].tap()
@@ -77,8 +46,90 @@ class Le_PlannerUITests: XCTestCase {
         
         
         // Use check to see if note was added
-        XCTAssert(tablesQuery.cells.count == 5)
+        XCTAssert(tablesQuery.cells.count == (rowsBeforeAdding + 1))
     }
+    
+    func testAddEvent() {
+        
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["Events"].tap()
+        
+        let rowsBeforeAdding = app.tables.cells.count
+        
+        app.navigationBars["Events"].buttons["Add"].tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        let titleTextField = elementsQuery.textFields["Title"]
+        titleTextField.tap()
+        titleTextField.typeText("Meeting")
+        
+        let descriptionTextField = elementsQuery.textFields["Description"]
+        descriptionTextField.tap()
+        descriptionTextField.typeText("Meeting John")
+        
+        let dateTextField = elementsQuery.textFields["Date"]
+        dateTextField.tap()
+        dateTextField.typeText("Today")
+        
+        let chooseEventTypeTextField = elementsQuery.textFields["Choose event type"]
+        chooseEventTypeTextField.tap()
+        chooseEventTypeTextField.typeText("Other")
+        
+        
+        app.navigationBars["Add Event"].buttons["Add"].tap()
+        app.alerts["Event Added"].buttons["OK"].tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .table).element.swipeDown()
+        
+        // Use check to see if event was added
+        let rowsAfterAdding = app.tables.cells.count
+        XCTAssert(rowsAfterAdding == (rowsBeforeAdding + 1))
+    }
+    
+    
+    func testDeleteEvent() {
+        // Use recording to get started writing UI tests.
+        
+        let app = XCUIApplication()
+        
+        app.tabBars.buttons["Events"].tap()
+        
+        let tablesQuery = app.tables
+        if tablesQuery.cells.count == 0 {
+            XCTAssert(true)
+        } else {
+            let rowsBeforeDeleting = tablesQuery.cells.count
+            let cell = tablesQuery.cells.allElementsBoundByIndex[0]
+            cell.swipeLeft()
+            tablesQuery.buttons["Delete"].tap()
+            
+            // Use check to see if event was deleted
+            XCTAssert(tablesQuery.cells.count == (rowsBeforeDeleting - 1))
+        }
+    }
+    
+    func testDeleteNote() {
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["Notes"].tap()
+        
+        let tablesQuery = app.tables
+        if tablesQuery.cells.count == 0 {
+            XCTAssert(true)
+        }
+        else {
+            let rowsBeforeDeleting = tablesQuery.cells.count
+            let noteEntry = tablesQuery.cells.allElementsBoundByIndex[0]
+            noteEntry.swipeLeft()
+            tablesQuery.buttons["Delete"].tap()
+            
+            // Use check to see if note was deleted
+            XCTAssert(tablesQuery.cells.count == (rowsBeforeDeleting - 1))
+        }
+        
+    }
+    
+    
     
     func testNavigation(){
         
@@ -90,7 +141,7 @@ class Le_PlannerUITests: XCTestCase {
         
         //Test to see if landed back on the home dashboard
         let app = XCUIApplication()
-        XCTAssert(app.staticTexts["  Quote of the day"].isEnabled)
+        XCTAssert(app.staticTexts["Quote of the day"].isEnabled)
         
         
     }
